6da14dc44c35abd6873ec02c6e87d8c0d01a4c58
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-10 01:09:38 +0700

    menambahkan form tambah mahasiswa

--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -13,6 +13,32 @@
 			    <label for="nama">Nama</label>
 			    <input type="text" class="form-control" id="nama">
 			  </div>
+			  <div class="form-group">
+			    <label for="nrp">Nrp</label>
+			    <input type="number" class="form-control" id="nrp">
+			  </div>
+			   <div class="form-group">
+			    <label for="email">Email</label>
+			    <input type="text" class="form-control" id="email">
+			  </div>
+			   <div class="form-group">
+				    <label for="jurusan">Jurusan</label>
+				    <select class="form-control" id="jurusan" name="jurusan">
+				      <option value="Teknik Informatika">Teknik Informatika</option>
+				      <option value="Teknik Industri">Teknik Industri</option>
+				      <option value="Teknik Pangan">Teknik Pangan</option>
+				      <option value="Teknik Planologi">Teknik Planologi</option>
+				      <option value="Teknik Mesin">Teknik Mesin</option>
+				      <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+				    </select>
+				  </div>
+
+
+				  	<div>
+				  		<button type="submit" name="tambah" class="btn btn-primary float-right" >Tambah</button>
+				  	</div>
+
+
 			</form>
  		 </div>
 </div>
